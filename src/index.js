b2706dc9b73175b68dae5c0689a0358ba8a24cc0
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2024-05-31 11:30:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const connectDB = require('./config/db');
 const mongoose = require('mongoose');
-require('dotenv').config();  // Load environment variables
+require('dotenv').config();  
 require('./config/db')
 const app = express();
 
-// Connect Database
-// connectDB();
 
-// Init Middleware
 app.use(bodyParser.json());
 
-// Define Routes
+
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/activity', require('./routes/activityRoutes'));
 
