89ba53f0a87ac59dbdf32e3f98ccfab60d90536a
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2024-10-09 09:53:06 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true); 
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb+srv://saad123:saad123@cluster0.98a5fer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("MongoDB connected...");
   })
